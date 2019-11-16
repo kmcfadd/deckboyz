f0c874967ef9b9888fc01e5cc4c191c06a196084
Kyle McFadden <burntwist@sbcglobal.net>
2019-11-16 11:59:16 -0800

    add enter key functionality to searchbar, create some dummy routes and models for decks/login

---
+++ controllers/deckController.js
@@ -0,0 +1,36 @@
+const db = require('../models');
+
+module.exports = {
+    findAllDecks: function(req, res) {
+        db.Deck
+            .find(req.query)
+            .then(stuff => res.json(stuff))
+            .catch(err => res.status(422).json(err))
+    },
+    findDeckById: function(req, res) {
+        db.Deck
+            .findById(req.params.id)
+            .then(stuff => res.json(stuff))
+            .catch(err => res.status(422).json(err))
+    },
+    createDeck: function(req, res) {
+        db.Deck
+            .create(req.body)
+            .then(stuff => res.json(stuff))
+            .catch(err => res.status(422).json(err))
+    },
+    updateDeck: function(req, res) {
+        db.Deck
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(stuff => res.json(stuff))
+            .catch(err => res.status(422).json(err))
+    },
+    deleteDeck: function(req, res) {
+        db.Deck
+            .findById({ _id: req.params.id })
+            .then(stuff => stuff.remove())
+            .then(stuff => res.json(stuff))
+            .catch(err => res.status(422).json(err))
+    }
+
+}
