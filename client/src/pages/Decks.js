c1ae32216d12515f759cd78936f4ba88678cd74d
Kyle McFadden <burntwist@sbcglobal.net>
2019-11-21 18:35:56 -0800

    added api call to get decks info from db and display them

--- client/src/pages/Decks.js
+++ client/src/pages/Decks.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import API from '../utils/API';
+import Wrapper from '../components/Wrapper/Wrapper';
 
 class Decks extends Component {
+    state = {
+        decks: [],
+    }
+
+    componentDidMount () {
+        API.getDecks()
+            .then(res => {
+                this.setState({ decks: res.data })
+            })
+            .catch(err => console.log(err))
+    }
 
     render() {
         return (
-            <div>
-                this is a Decks page
-            </div>
-    )
-}
-    
+            <Wrapper>
+                <ul>
+                    {this.state.decks.map(deck => (
+                        <li style={{listStyleType: 'none'}}>
+                            <strong>{deck.deckName} by {deck.createdBy}</strong><br />
+                            <i>Submitted {deck.createdOn}</i> {/* need to format this with moment or something */}
+                            <li style={{listStyleType: 'none'}}>
+                                {deck.deck.map(card => (
+                                    <p>{card.name} x {card.copies}</p>
+                                ))}
+                            </li>
+                        </li>
+                ))}
+                </ul> 
+            </Wrapper>
+        )
+    }   
 }
 
 export default Decks;
