68c9e7a948b26c6e274eed0e1c703cede633c918
MrJoggington <49696148+MrJoggington@users.noreply.github.com>
2019-11-22 17:31:28 -0800

    made list into modals, this breaks it since we can no longer view the card list for the specified decks

--- client/src/pages/Decks.js
+++ client/src/pages/Decks.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import Navbar from '../components/Navbar/navbar'
 import Wrapper from '../components/Wrapper/Wrapper';
 import API from '../utils/API';
+import Garruk from '../components/image/Headofmen.jpg'
 
 class Decks extends Component {
     state = {
         decks: [],
+        isBoxVisible: false,
     }
 
-    componentDidMount () {
+
+    componentDidMount() {
         API.getDecks()
             .then(res => {
                 this.setState({ decks: res.data })
@@ -16,26 +19,58 @@ class Decks extends Component {
             .catch(err => console.log(err))
     }
 
+    toggleBox = () => {
+        this.setState(prevState => ({ isBoxVisible: !prevState.isBoxVisible }))
+
+    }
+
     render() {
+        const { isBoxVisible } = this.state
+        console.log(this.state.decks)
         return (
-            <div>
+            < div >
                 <Navbar />
-                <Wrapper>
-                <ul>
-                    {this.state.decks.map(deck => (
-                        <li style={{listStyleType: 'none'}}>
-                            <strong>{deck.deckName} by {deck.createdBy}</strong><br />
-                            <i>Submitted {deck.createdOn}</i> {/* need to format this with moment or something */}
-                            <li style={{listStyleType: 'none'}}>
-                                {deck.deck.map(card => (
-                                    <p>{card.name} x {card.copies}</p>
-                                ))}
-                            </li>
-                        </li>
-                ))}
-                </ul> 
-            </Wrapper>   
-            </div>
+                <div className="hero is-info is-bold">
+                    <div className="hero-body">
+                        <div className="media">
+                            <img className="media-left" src={Garruk} alt="head of men" style={{ width: "100px", borderRadius: "50%" }} />
+                            <h1 className="media-content">
+                                <strong className="title">Manly Man Garruk Says </strong>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="container is-fluid">
+                    <Wrapper>
+                        <ul className="columns" style={{ display: "flex", flexFlow: 'row wrap' }}>
+
+                            {this.state.decks.map(deck => (
+                                <div>
+
+                                    <li className="column" onClick={this.toggleBox} style={{ listStyleType: 'none' }}>
+                                        <strong>{deck.deckName} by {deck.createdBy}</strong><br />
+                                        <i>Submitted {deck.createdOn}</i>
+                                        {/*Moment js this sucka */}
+                                    </li>
+
+                                    < li className={`modal ${isBoxVisible ? "is-active" : " "}`} style={{ listStyleType: 'none' }}>
+                                        <div className="modal-background">
+                                            {deck.deck.map(card => (
+                                                <p className="modal-content">{
+                                                    card.name} x {card.copies}</p>
+                                            ))}
+                                            <button className="modal-close" onClick={this.toggleBox}></button>
+                                        </div>
+                                    </li>
+
+                                </div>
+
+                            ))}
+                        </ul>
+
+                    </Wrapper>
+                </div>
+            </div >
         )
     }
 }
